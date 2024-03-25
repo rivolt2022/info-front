5cb6565e11a24d02581aa4d5f651637c6656dd3e
이경민 <km0118@yonsei.ac.kr>
2024-03-25 22:41:57 +0900

    test

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -68,7 +68,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <Link href="/">
-          <span className="text-lg text-white">성과관리시스템</span>
+          <img src="/images/logo/yonsei_logo.png" className="h-12 inline-flex"></img>
         </Link>
 
         <button
